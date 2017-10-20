d20aa4a5fed9be6521797d09e54efe766a5456be
Rakesh Shrestha <rakeshsh31@gmail.com>
2017-10-20 15:07:17 -0700

    error calculation for EuRoC dataset

--- evaluate_ate_scale.py
+++ evaluate_ate_scale.py
@@ -43,10 +43,23 @@ This script computes the absolute trajectory error from the ground truth
 trajectory and the estimated trajectory.
 """
 
+from __future__ import print_function
 import sys
 import numpy
 import argparse
+import yaml
+
 import associate
+import transformations
+
+def eprint(*args, **kwargs):
+    """
+    print to stderr instead of stdout
+    :param args: same as that in print
+    :param kwargs: same as that in print
+    :return:
+    """
+    print(*args, file=sys.stderr, **kwargs)
 
 def align(model,data):
     """Align two trajectories using the method of Horn (closed-form).
@@ -85,7 +98,7 @@ def align(model,data):
 
     s = float(dots/norms)    
 
-    print "scale: %f " % s  
+    print("scale: %f " % s)
     
     trans = data.mean(1) - s*rot * model.mean(1)
     
@@ -126,18 +139,76 @@ def plot_traj(ax,stamps,traj,style,color,label):
         last= stamps[i]
     if len(x)>0:
         ax.plot(x,y,style,color=color,label=label)
-            
+
+def get_transformation(config_file):
+    """
+    Gets transformation from body frame (imu) to sensor frame (camera)
+    :param config_file: the sensor.yaml file containing sensor to body transformation
+    :return: the transformation matrix if successful, None if not
+    """
+    config = None
+    with open(config_file, 'r') as stream:
+        config = yaml.load(stream)
+
+    if not config:
+        eprint("bad config file")
+        return None
+
+    if (not 'T_BS' in config) or (not 'data' in config['T_BS']):
+        eprint("required keys not in the config file")
+        return None
+    T_BS = numpy.asarray(config['T_BS']['data'])
+    T_BS = numpy.resize(T_BS, (4, 4))
+
+    T_SB = numpy.linalg.inv(T_BS)
+    return T_SB
+
+def convert_groundtruth(groundtruth, T_SB):
+    """
+    Convert groundtruth from EuRoC format to TUM format (and transform it to sensor (camera frame) from body frame
+    :param groundtruth: a dictionary of groundtruth data in the EuRoC format timestamp(ns),tx,ty,tz,qw,qx,qy,qz)
+    :param T_SB: a two tuple of rotation (quaternion) and translation representing transformation from body to sensor frame
+    :return: equivalent dictionary in TUM-RGBD format timestamp(s) tx ty tz qx qy qz qw
+    """
+
+    def euroc_pose_2_transform(euroc_pose):
+        """
+        Convert from EuRoC pose format to transformation matrix
+        :param euroc_pose: tx,ty,tz,qw,qx,qy,qz
+        :return: equivalent transformation matrix
+        """
+        T = transformations.quaternion_matrix(euroc_pose[3:7])
+        T[:3, 3] = euroc_pose[:3]
+        return T
+
+    def transformation_2_tum_pose(T):
+        """
+        Convert from transformation matrix to TUM-RGBD dataset pose
+        :param T: transformation matrix
+        :return: tum pose: tx ty tz qx qy qz qw
+        """
+        q = transformations.quaternion_from_matrix(T)
+        return [
+            T[0, 3], T[1, 3],T[2, 3],
+            q[1], q[2], q[3], q[0] # x,y,z,w
+        ]
+
+    return {
+        (i*1e-9): transformation_2_tum_pose( numpy.dot(T_SB, euroc_pose_2_transform(groundtruth[i][:7])) )
+        for i in groundtruth
+    }
 
 if __name__=="__main__":
     # parse command line
     parser = argparse.ArgumentParser(description='''
     This script computes the absolute trajectory error from the ground truth trajectory and the estimated trajectory. 
     ''')
-    parser.add_argument('first_file', help='ground truth trajectory (format: timestamp tx ty tz qx qy qz qw)')
-    parser.add_argument('second_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('first_file', help='ground truth trajectory (format: timestamp(ns),tx,ty,tz,qw,qx,qy,qz)')
+    parser.add_argument('second_file', help='estimated trajectory (format: timestamp(s) tx ty tz qx qy qz qw)')
+    parser.add_argument('third_file', help='yaml file with key "Camera.Tbc" containing the transformation matrix from camera to body frame of ground truth in the row-major form')
     parser.add_argument('--offset', help='time offset added to the timestamps of the second file (default: 0.0)',default=0.0)
     parser.add_argument('--scale', help='scaling factor for the second trajectory (default: 1.0)',default=1.0)
-    parser.add_argument('--max_difference', help='maximally allowed time difference for matching entries (default: 0.02)',default=0.02)
+    parser.add_argument('--max_difference', help='maximally allowed time difference for matching entries (default: 0.03)',default=0.03)
     parser.add_argument('--save', help='save aligned second trajectory to disk (format: stamp2 x2 y2 z2)')
     parser.add_argument('--save_associations', help='save associated first and aligned second trajectory to disk (format: stamp1 x1 y1 z1 stamp2 x2 y2 z2)')
     parser.add_argument('--plot', help='plot the first and the aligned second trajectory to an image (format: png)')
@@ -147,7 +218,13 @@ if __name__=="__main__":
     first_list = associate.read_file_list(args.first_file)
     second_list = associate.read_file_list(args.second_file)
 
-    matches = associate.associate(first_list, second_list,float(args.offset),float(args.max_difference))    
+    # the transformation from body frame to sensor frame
+    T_SB = get_transformation(args.third_file)
+
+    # convert from EuRoC to TUM-RGBD format
+    first_list = convert_groundtruth(first_list, T_SB)
+
+    matches = associate.associate(first_list, second_list,float(args.offset),float(args.max_difference))
     if len(matches)<2:
         sys.exit("Couldn't find matching timestamp pairs between groundtruth and estimated trajectory! Did you choose the correct sequence?")
 
@@ -168,16 +245,16 @@ if __name__=="__main__":
     second_xyz_full_aligned = scale * rot * second_xyz_full + trans
     
     if args.verbose:
-        print "compared_pose_pairs %d pairs"%(len(trans_error))
+        print("compared_pose_pairs %d pairs"%(len(trans_error)))
 
-        print "absolute_translational_error.rmse %f m"%numpy.sqrt(numpy.dot(trans_error,trans_error) / len(trans_error))
-        print "absolute_translational_error.mean %f m"%numpy.mean(trans_error)
-        print "absolute_translational_error.median %f m"%numpy.median(trans_error)
-        print "absolute_translational_error.std %f m"%numpy.std(trans_error)
-        print "absolute_translational_error.min %f m"%numpy.min(trans_error)
-        print "absolute_translational_error.max %f m"%numpy.max(trans_error)
+        print("absolute_translational_error.rmse %f m"%numpy.sqrt(numpy.dot(trans_error,trans_error) / len(trans_error)))
+        print("absolute_translational_error.mean %f m"%numpy.mean(trans_error))
+        print("absolute_translational_error.median %f m"%numpy.median(trans_error))
+        print("absolute_translational_error.std %f m"%numpy.std(trans_error))
+        print("absolute_translational_error.min %f m"%numpy.min(trans_error))
+        print("absolute_translational_error.max %f m"%numpy.max(trans_error))
     else:
-        print "%f"%numpy.sqrt(numpy.dot(trans_error,trans_error) / len(trans_error))
+        print("%f"%numpy.sqrt(numpy.dot(trans_error,trans_error) / len(trans_error)))
         
     if args.save_associations:
         file = open(args.save_associations,"w")
